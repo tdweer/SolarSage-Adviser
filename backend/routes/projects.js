9fac21b808afba8fae6860c73ccfd1b7e3b4ddda
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-01-16 20:40:35 +0530

    routes 1

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -2,10 +2,29 @@ const express = require('express')
 
 const router = express.Router()
 
+//get all projects
 router.get('/', (req, res) => {
     res.json({ message: 'Get all projects '})
 })
 
+//get one project
+router.get('/:id', (req, res) => {
+    res.json({ message: 'Get one project '})
+})
 
+//post a new project
+router.post('/', (req, res) => {
+    res.json({ message: 'Post a new project '})
+})
+
+//delete a project
+router.delete('/:id', (req, res) => {
+    res.json({ message: 'Delete a project '})
+})
+
+//update a project
+router.patch('/:id', (req, res) => {
+    res.json({ message: 'Update a project '})
+})
 
 module.exports = router
