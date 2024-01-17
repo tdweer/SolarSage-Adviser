840d49ea459d4f0d56b5b7cee62187fa37706329
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-01-17 18:43:45 +0530

    models & schemas

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 
 const express = require('express')
+const mongoose = require('mongoose')
 const projectRoutes = require('./routes/projects')
 
 //express app
@@ -18,14 +19,24 @@ app.use((req,res,next) => {
     next()
 })
 
+//connect to mongodb
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+         console.log('Connected to the db & listening on port, ', process.env.PORT);
+})
+
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+
 // //routes
 app.use('/api/projects',projectRoutes)
 
 
 
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port, ', process.env.PORT);
-})
 
