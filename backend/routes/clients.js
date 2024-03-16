608876ec00dd98ce15c8d0729ab7c480540483f3
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-03-17 01:10:44 +0530

    redesigning

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -6,7 +6,7 @@ const {
     deleteClient,
     updateClient
 } = require('../controllers/clientController')
- const  requireAuth  = require('../middleware/requireAuth')
+// const  requireAuth  = require('../middleware/requireAuth')
 
 
 
@@ -15,7 +15,7 @@ const {
 const router = express.Router()
 
 
- router.use(requireAuth)
+// router.use(requireAuth)
 
 //Get all clients
 router.get('/', getClients)
