0de016b7ca534510b4426653fac4ac308b8424a1
Tharindu Weerasekara <tdasunrcm@gmail.com>
2024-05-08 18:31:00 +0530

    deleted

--- frontend/src/pages/admin/admin-pages/Adviser.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * PROJECT NAME      : solar-system-fe
- * PROJECT AUTHOR    : MINDULA DILTHUSHAN
- * AUTHOR EMAIL      : minduladilthushan1@gmail.com
- * DATE              : 05/05/2024
- *
- */
-
